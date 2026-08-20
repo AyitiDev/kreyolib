86d7873cdaf721ecf8f4fc2b71de1dc8f45a775e
speedyk-005 <116703678+speedyk-005@users.noreply.github.com>
2026-08-20 17:54:30 -0400

    test: add test for pos tagging

---
+++ tests/test_pos_tagging.py
@@ -0,0 +1,53 @@
+import pytest
+
+from kreyolib.tagger.pos import tag
+
+
+@pytest.mark.parametrize(
+    ("inputs, expected"),
+    [
+        ("", []),
+        (
+            "Map vini demen nan maten.",
+            [
+                ("M", "PRON"),
+                ("ap", "AUX"),
+                ("vini", "VERB"),
+                ("demen", "NOUN"),
+                ("nan", "ADP"),
+                ("maten", "NOUN"),
+                (".", "PUNCT"),
+            ],
+        ),
+        (
+            "Lap travay pandan tout jounen an.",
+            [
+                ("L", "PRON"),
+                ("ap", "AUX"),
+                ("travay", "VERB"),
+                ("pandan", "ADP"),
+                ("tout", "DET"),
+                ("jounen", "NOUN"),
+                ("an", "DET"),
+                (".", "PUNCT"),
+            ],
+        ),
+        (
+            ["Mwen", "rele", "Jan", ",", "e", "mwen", "abite", "Okay", "."],
+            [
+                ("Mwen", "PRON"),
+                ("rele", "VERB"),
+                ("Jan", "PROPN"),
+                (",", "PUNCT"),
+                ("e", "CCONJ"),
+                ("mwen", "PRON"),
+                ("abite", "VERB"),
+                ("Okay", "NOUN"),
+                (".", "PUNCT"),
+            ],
+        ),
+    ],
+)
+def test_pos_tagging(inputs, expected):
+    """Test POS tagger with extended string and tokenized inputs."""
+    assert tag(inputs) == expected
